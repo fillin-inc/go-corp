301de4b7cc32e99048433f5992b9551d325acdc0
Yuya Matsushima <yuyam@fillin-inc.com>
2021-07-17 08:01:04 +0900

    Date#Time のテストを追加

--- date_test.go
+++ date_test.go
@@ -2,6 +2,7 @@ package corp
 
 import (
 	"encoding/xml"
+	"reflect"
 	"testing"
 	"time"
 )
@@ -27,5 +28,14 @@ func TestUnmarshalDate(t *testing.T) {
 	expected := time.Date(2021, 7, 16, 0, 0, 0, 0, loc).Unix()
 	if dt.Result.Time().Unix() != expected {
 		t.Errorf("XMLのパース結果に問題があります。result:%v, expected:%v", dt.Result.Time().Unix(), expected)
+	}
+}
+
+func TestDateToTime(t *testing.T) {
+	time := time.Now()
+	d := Date(time)
+
+	if reflect.TypeOf(d.Time()) != reflect.TypeOf(time) {
+		t.Errorf("型情報に誤りがあります。result:%T expected:%T", d.Time(), time)
 	}
 }
