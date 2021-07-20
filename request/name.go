0e4592fb2aa5cad76e2ac6e911c68fd58464a98a
Yuya Matsushima <yuyam@fillin-inc.com>
2021-07-20 16:24:19 +0900

    request.Name を追加

---
+++ request/name.go
@@ -0,0 +1,78 @@
+package request
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/go-playground/validator"
+	"github.com/google/go-querystring/query"
+)
+
+// 法人名検索
+type Name struct {
+	// アプリケーションID
+	ID string `validate:"required" url:"id"`
+	// 商号または名称
+	Name string `validate:"required" url:"name"`
+	// 検索方式
+	Mode int `validate:"min=1,max=2" url:"mode,omitempty"`
+	// 検索対象
+	Target int `validate:"min=1,max=3" url:"target"`
+	// 所在地
+	Address string `validate:"address" url:"address,omitempty"`
+	// 法人種別
+	Kind []string `validate:"max=4,kind" url:"kind,omitempty" del:","`
+	// 変更履歴
+	Change bool `url:"change,int"`
+	// 閉鎖登記取得
+	Close bool `url:"close,int"`
+	// 法人番号指定年月日開始日
+	From string `validate:"date" url:"from,omitempty"`
+	// 法人番号指定年月日終了日
+	To string `validate:"date,gtedate=From" url:"to,omitempty"`
+	// 分割番号
+	Divide int `validate:"min=1,max=99999" url:"divide"`
+	// 応答形式
+	ResponseType string `validate:"required,eq=12" url:"type"`
+}
+
+func NewName(appID string, name string, mode int, target int, address string, kind []string, change bool, close bool, from string, to string, divide int) *Name {
+	return &Name{
+		appID,
+		name,
+		mode,
+		target,
+		address,
+		kind,
+		change,
+		close,
+		from,
+		to,
+		divide,
+		RESPONSE_TYPE,
+	}
+}
+
+func (n Name) Validate() error {
+	validator := validator.New()
+	validator.RegisterValidation("date", dateValidation)
+	validator.RegisterValidation("gtedate", dateEqualOrGreaterValidation)
+	validator.RegisterValidation("address", addressValidation)
+	validator.RegisterValidation("kind", kindValidation)
+	return validator.Struct(n)
+}
+
+func (n Name) URL() (url.URL, error) {
+	var u url.URL
+	q, err := query.Values(n)
+	if err != nil {
+		return u, fmt.Errorf("failed to convert query string: %w", err)
+	}
+
+	return url.URL{
+		Scheme:   "https",
+		Host:     HOST,
+		Path:     fmt.Sprintf("/%d/name", API_VER),
+		RawQuery: q.Encode(),
+	}, nil
+}
---
+++ request/name_test.go
@@ -0,0 +1,775 @@
+package request
+
+import (
+	"reflect"
+	"testing"
+)
+
+type nameValidationErrTest struct {
+	Name   Name
+	ErrMsg string
+}
+
+type nameURLTest struct {
+	Name Name
+	URL  string
+}
+
+func TestNewName(t *testing.T) {
+	appID := "ABCDEFG"
+	name := "フィルイン"
+	mode := 1
+	target := 1
+	address := "10202"
+	kind := []string{"03"}
+	change := false
+	close := false
+	from := "2016-09-01"
+	to := "2016-09-10"
+	divide := 2
+
+	n := NewName(appID, name, mode, target, address, kind, change, close, from, to, divide)
+
+	if n.ID != appID {
+		t.Error("ID is not match")
+	}
+
+	if n.Name != name {
+		t.Error("Name is not match")
+	}
+
+	if n.Mode != mode {
+		t.Error("Mode is not match")
+	}
+
+	if n.Target != target {
+		t.Error("Target is not match")
+	}
+
+	if n.Address != address {
+		t.Error("Address is not match")
+	}
+
+	if !reflect.DeepEqual(n.Kind, kind) {
+		t.Error("Kind is not match")
+	}
+
+	if n.Change != change {
+		t.Error("Change is not match")
+	}
+
+	if n.Close != close {
+		t.Error("Close is not match")
+	}
+
+	if n.From != from {
+		t.Error("From is not match")
+	}
+
+	if n.To != to {
+		t.Error("To is not match")
+	}
+
+	if n.Divide != divide {
+		t.Error("Devide is not match")
+	}
+
+	if n.ResponseType != RESPONSE_TYPE {
+		t.Error("ResponseType is not match")
+	}
+}
+
+func TestNameValidate(t *testing.T) {
+	tests := []Name{
+		{
+			ID:           "ABCDEFG",
+			Name:         "フィルイン",
+			Mode:         1,
+			Target:       1,
+			Address:      "",
+			Kind:         []string{},
+			Change:       false,
+			Close:        false,
+			From:         "",
+			To:           "",
+			Divide:       1,
+			ResponseType: RESPONSE_TYPE,
+		},
+		// Mode is 2
+		{
+			ID:           "ABCDEFG",
+			Name:         "フィルイン",
+			Mode:         2,
+			Target:       1,
+			Address:      "",
+			Kind:         []string{},
+			Change:       false,
+			Close:        false,
+			From:         "",
+			To:           "",
+			Divide:       1,
+			ResponseType: RESPONSE_TYPE,
+		},
+		// Target is 2
+		{
+			ID:           "ABCDEFG",
+			Name:         "フィルイン",
+			Mode:         1,
+			Target:       2,
+			Address:      "",
+			Kind:         []string{},
+			Change:       false,
+			Close:        false,
+			From:         "",
+			To:           "",
+			Divide:       1,
+			ResponseType: RESPONSE_TYPE,
+		},
+		// Target is 3
+		{
+			ID:           "ABCDEFG",
+			Name:         "フィルイン",
+			Mode:         1,
+			Target:       3,
+			Address:      "",
+			Kind:         []string{},
+			Change:       false,
+			Close:        false,
+			From:         "",
+			To:           "",
+			Divide:       1,
+			ResponseType: RESPONSE_TYPE,
+		},
+		// Address is PrefCode
+		{
+			ID:           "ABCDEFG",
+			Name:         "フィルイン",
+			Mode:         1,
+			Target:       1,
+			Address:      "10",
+			Kind:         []string{},
+			Change:       false,
+			Close:        false,
+			From:         "",
+			To:           "",
+			Divide:       1,
+			ResponseType: RESPONSE_TYPE,
+		},
+		// Address has PrefCode + CityCode
+		{
+			ID:           "ABCDEFG",
+			Name:         "フィルイン",
+			Mode:         1,
+			Target:       1,
+			Address:      "10202",
+			Kind:         []string{},
+			Change:       false,
+			Close:        false,
+			From:         "",
+			To:           "",
+			Divide:       1,
+			ResponseType: RESPONSE_TYPE,
+		},
+		// Address has 1 KindCode
+		{
+			ID:           "ABCDEFG",
+			Name:         "フィルイン",
+			Mode:         1,
+			Target:       1,
+			Address:      "",
+			Kind:         []string{"03"},
+			Change:       false,
+			Close:        false,
+			From:         "",
+			To:           "",
+			Divide:       1,
+			ResponseType: RESPONSE_TYPE,
+		},
+		// Kind has 2 KindCode
+		{
+			ID:           "ABCDEFG",
+			Name:         "フィルイン",
+			Mode:         1,
+			Target:       1,
+			Address:      "",
+			Kind:         []string{"01", "03"},
+			Change:       false,
+			Close:        false,
+			From:         "",
+			To:           "",
+			Divide:       1,
+			ResponseType: RESPONSE_TYPE,
+		},
+		// Change is true
+		{
+			ID:           "ABCDEFG",
+			Name:         "フィルイン",
+			Mode:         1,
+			Target:       1,
+			Address:      "",
+			Kind:         []string{},
+			Change:       true,
+			Close:        false,
+			From:         "",
+			To:           "",
+			Divide:       1,
+			ResponseType: RESPONSE_TYPE,
+		},
+		// Close is true
+		{
+			ID:           "ABCDEFG",
+			Name:         "フィルイン",
+			Mode:         1,
+			Target:       1,
+			Address:      "",
+			Kind:         []string{},
+			Change:       false,
+			Close:        true,
+			From:         "",
+			To:           "",
+			Divide:       1,
+			ResponseType: RESPONSE_TYPE,
+		},
+		// From has value
+		{
+			ID:           "ABCDEFG",
+			Name:         "フィルイン",
+			Mode:         1,
+			Target:       1,
+			Address:      "",
+			Kind:         []string{},
+			Change:       false,
+			Close:        true,
+			From:         "2016-09-01",
+			To:           "",
+			Divide:       1,
+			ResponseType: RESPONSE_TYPE,
+		},
+		// To have value
+		{
+			ID:           "ABCDEFG",
+			Name:         "フィルイン",
+			Mode:         1,
+			Target:       1,
+			Address:      "",
+			Kind:         []string{},
+			Change:       false,
+			Close:        true,
+			From:         "",
+			To:           "2016-09-10",
+			Divide:       1,
+			ResponseType: RESPONSE_TYPE,
+		},
+		// From and To have same value
+		{
+			ID:           "ABCDEFG",
+			Name:         "フィルイン",
+			Mode:         1,
+			Target:       1,
+			Address:      "",
+			Kind:         []string{},
+			Change:       false,
+			Close:        true,
+			From:         "2016-09-01",
+			To:           "2016-09-01",
+			Divide:       1,
+			ResponseType: RESPONSE_TYPE,
+		},
+		// From and To have different value
+		{
+			ID:           "ABCDEFG",
+			Name:         "フィルイン",
+			Mode:         1,
+			Target:       1,
+			Address:      "",
+			Kind:         []string{},
+			Change:       false,
+			Close:        true,
+			From:         "2016-09-01",
+			To:           "2016-09-10",
+			Divide:       1,
+			ResponseType: RESPONSE_TYPE,
+		},
+		// Divide has max value(99999)
+		{
+			ID:           "ABCDEFG",
+			Name:         "フィルイン",
+			Mode:         1,
+			Target:       1,
+			Address:      "",
+			Kind:         []string{},
+			Change:       false,
+			Close:        true,
+			From:         "",
+			To:           "",
+			Divide:       99999,
+			ResponseType: RESPONSE_TYPE,
+		},
+	}
+
+	for i, name := range tests {
+		if err := name.Validate(); err != nil {
+			t.Errorf("%d: Validation Error %v", i, err)
+		}
+	}
+}
+
+func TestNameValidateError(t *testing.T) {
+	tests := []nameValidationErrTest{
+		{
+			// ID is empty
+			Name{
+				ID:           "",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"Key: 'Name.ID' Error:Field validation for 'ID' failed on the 'required' tag",
+		},
+		{
+			// Name is empty
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "",
+				Mode:         1,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"Key: 'Name.Name' Error:Field validation for 'Name' failed on the 'required' tag",
+		},
+		{
+			// Mode is zero
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         0,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"Key: 'Name.Mode' Error:Field validation for 'Mode' failed on the 'min' tag",
+		},
+		{
+			// Mode is 3
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         3,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"Key: 'Name.Mode' Error:Field validation for 'Mode' failed on the 'max' tag",
+		},
+		{
+			// Target is zero
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       0,
+				Address:      "",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"Key: 'Name.Target' Error:Field validation for 'Target' failed on the 'min' tag",
+		},
+		{
+			// Target is 4
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       4,
+				Address:      "",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"Key: 'Name.Target' Error:Field validation for 'Target' failed on the 'max' tag",
+		},
+		{
+			// Address is invalid PrefCode
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "48",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"Key: 'Name.Address' Error:Field validation for 'Address' failed on the 'address' tag",
+		},
+		{
+			// Address is invalid PrefCode + CityCode
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "00202",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"Key: 'Name.Address' Error:Field validation for 'Address' failed on the 'address' tag",
+		},
+		{
+			// Address is invalid format
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "TEST1",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"Key: 'Name.Address' Error:Field validation for 'Address' failed on the 'address' tag",
+		},
+		{
+			// Address is invalid CityCode
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "99TES",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"Key: 'Name.Address' Error:Field validation for 'Address' failed on the 'address' tag",
+		},
+		{
+			// Kind contains invalid KindCode
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{"05", "03"},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"Key: 'Name.Kind' Error:Field validation for 'Kind' failed on the 'kind' tag",
+		},
+		{
+			// From is invalid format
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "2021-07",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"Key: 'Name.From' Error:Field validation for 'From' failed on the 'date' tag",
+		},
+		{
+			// To is invalid format
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "2021-07",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"Key: 'Name.To' Error:Field validation for 'To' failed on the 'date' tag",
+		},
+		{
+			// To is past than From
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "2021-07-19",
+				To:           "2021-07-18",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"Key: 'Name.To' Error:Field validation for 'To' failed on the 'gtedate' tag",
+		},
+		{
+			// Divide is less than min value(1)
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       0,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"Key: 'Name.Divide' Error:Field validation for 'Divide' failed on the 'min' tag",
+		},
+		{
+			// Divide is greater than max value(99999)
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       100000,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"Key: 'Name.Divide' Error:Field validation for 'Divide' failed on the 'max' tag",
+		},
+		{
+			// ResponseType is empty
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: "",
+			},
+			"Key: 'Name.ResponseType' Error:Field validation for 'ResponseType' failed on the 'required' tag",
+		},
+		{
+			// ResponseType is invalid
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: "01",
+			},
+			"Key: 'Name.ResponseType' Error:Field validation for 'ResponseType' failed on the 'eq' tag",
+		},
+	}
+
+	for i, test := range tests {
+		err := test.Name.Validate()
+		if err == nil {
+			t.Errorf("%d: Validation Error not returns", i)
+		}
+
+		if err.Error() != test.ErrMsg {
+			t.Errorf("%d: Validation Error Message not matched result:%s expected:%s", i, err.Error(), test.ErrMsg)
+		}
+	}
+}
+
+func TestNameURL(t *testing.T) {
+	tests := []nameURLTest{
+		{
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"https://api.houjin-bangou.nta.go.jp/4/name?change=0&close=0&divide=1&id=ABCDEFG&mode=1&name=%E3%83%95%E3%82%A3%E3%83%AB%E3%82%A4%E3%83%B3&target=1&type=12",
+		},
+		{
+			// Address is specified
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "10202",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"https://api.houjin-bangou.nta.go.jp/4/name?address=10202&change=0&close=0&divide=1&id=ABCDEFG&mode=1&name=%E3%83%95%E3%82%A3%E3%83%AB%E3%82%A4%E3%83%B3&target=1&type=12",
+		},
+		{
+			// Kind is provided 1 KindCode
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{"03"},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"https://api.houjin-bangou.nta.go.jp/4/name?change=0&close=0&divide=1&id=ABCDEFG&kind=03&mode=1&name=%E3%83%95%E3%82%A3%E3%83%AB%E3%82%A4%E3%83%B3&target=1&type=12",
+		},
+		{
+			// Kind is provided 3 KindCode
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{"01", "02", "03"},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"https://api.houjin-bangou.nta.go.jp/4/name?change=0&close=0&divide=1&id=ABCDEFG&kind=01%2C02%2C03&mode=1&name=%E3%83%95%E3%82%A3%E3%83%AB%E3%82%A4%E3%83%B3&target=1&type=12",
+		},
+		{
+			// From is specified
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "2021-07-19",
+				To:           "",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"https://api.houjin-bangou.nta.go.jp/4/name?change=0&close=0&divide=1&from=2021-07-19&id=ABCDEFG&mode=1&name=%E3%83%95%E3%82%A3%E3%83%AB%E3%82%A4%E3%83%B3&target=1&type=12",
+		},
+		{
+			// To is specified
+			Name{
+				ID:           "ABCDEFG",
+				Name:         "フィルイン",
+				Mode:         1,
+				Target:       1,
+				Address:      "",
+				Kind:         []string{},
+				Change:       false,
+				Close:        false,
+				From:         "",
+				To:           "2021-07-19",
+				Divide:       1,
+				ResponseType: RESPONSE_TYPE,
+			},
+			"https://api.houjin-bangou.nta.go.jp/4/name?change=0&close=0&divide=1&id=ABCDEFG&mode=1&name=%E3%83%95%E3%82%A3%E3%83%AB%E3%82%A4%E3%83%B3&target=1&to=2021-07-19&type=12",
+		},
+	}
+
+	for i, test := range tests {
+		url, _ := test.Name.URL()
+
+		if url.String() != test.URL {
+			t.Errorf("%d: URL String not match result:%s expected:%s", i, url.String(), test.URL)
+		}
+	}
+}
